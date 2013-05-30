9c1327f36aa190b739614502ceb8d6c6197e6ba8
ataylor <github@rxtaylor.com>
2013-05-30 17:05:55 -0400

    placeholders

--- newinvoice.php
+++ newinvoice.php
@@ -8,7 +8,7 @@
                 <colgroup><col id="label" style="width:20%;"><col id="box" style="width:70%"></colgroup>
                 <tr>
                     <td>Search by Name or NDC:</td>
-                    <td><input type="text" name="inputStringBox" id="inputStringBox" autocomplete="off" onkeyup="searchbox(this.value,'invoice');" style="width:90%;" autofocus /></td>
+                    <td><input type="text" name="inputStringBox" id="inputStringBox" autocomplete="off" onkeyup="searchbox(this.value,'invoice');" style="width:90%;" autofocus placeholder="Enter Brand Name, Generic Name, or NDC" /></td>
                 </tr>            
             </table>
         </form>
