006567da00e32de0d8684f3574ce712fdd6b0425
ataylor <github@rxtaylor.com>
2013-05-30 17:01:14 -0400

    Added autofocus attribute to certain inputs

--- newrx.php
+++ newrx.php
@@ -8,7 +8,7 @@
                 <colgroup><col id="label" style="width:20%;"><col id="box" style="width:70%"></colgroup>
                 <tr>
                     <td>Search by Name or NDC:</td>
-                    <td><input type="text" name="inputStringBox" id="inputStringBox" autocomplete="off" onkeyup="searchbox(this.value,'rx');" style="width:90%;" /></td>
+                    <td><input type="text" name="inputStringBox" id="inputStringBox" autocomplete="off" onkeyup="searchbox(this.value,'rx');" style="width:90%;" autofocus/></td>
                 </tr>            
             </table>
         </form>
