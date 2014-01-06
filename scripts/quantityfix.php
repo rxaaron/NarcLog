e41b880f06b024b33f71184f67b840551e83ab71
rxaaron <github@rxtaylor.com>
2014-01-05 19:50:07 -0500

    error correct

--- scripts/quantityfix.php
+++ scripts/quantityfix.php
@@ -7,7 +7,7 @@
     <body>
         <form name="onhandfix" id="onhandfix" action="quick_updateonhand.php" method="POST" autocompete="off">
             <table name="newonhand" id="newonhand">
-                <colgroup><col name="label" style="width:200px;"><col name="boxes" style="500px;"></colgroup>
+                <colgroup><col name="label" style="width:200px;"><col name="boxes" style="width:500px;"></colgroup>
                 <tr><td>Drug:</td><td><?php include_once('scripts/quick_druglist.php'); ?></td></tr>
                 <tr><td>New Onhand:</td><td><input type="text" name="onhand" /></td></tr>
             </table>
